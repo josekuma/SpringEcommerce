74d6cd94a5a2be2b9532273d13baa5b2ab72d668
josecgj <josekgj@gmail.com>
2022-11-08 17:49:03 +0100

    creacion de la estructura y las clases model

---
+++ eclipse-workspace/ecommerceProyecto/src/test/java/com/josek/ecommerce/EcommerceProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josek.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
